fe9d0d126e1233f8bf648dd4c25844f7d3730e1f
Léon Geide <leon.geide@mail.de>
2020-05-01 01:25:58 +0200

    Database and table added

--- ScheduleHandler/ScheduleHandler.py
+++ ScheduleHandler/ScheduleHandler.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+print("Now we are creating a table within a database...") # 
+
+import sqlite3 
+connection = sqlite3.connect("any.db") 
+cursor = connection.cursor() 
+
+cursor.execute("""
+    CREATE TABLE employee (
+    staff_number INTEGER PRINMARY KEY, 
+    fname VARCHAR(20)
+    );"""
+    )
+
+cursor.execute("""DROP TABLE employee;""")
