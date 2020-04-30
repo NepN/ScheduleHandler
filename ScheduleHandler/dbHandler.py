53d6796f879966c1111dd6fc8f948ea4b2235081
Léon Geide <leon.geide@mail.de>
2020-05-01 01:49:15 +0200

    Finetuning functions - housekeeping if table exists

--- ScheduleHandler/dbHandler.py
+++ ScheduleHandler/dbHandler.py
@@ -6,11 +6,11 @@ def ConnectDB(): # creates db if not existing
     return cursor; # return value of 'cursor' to line of fnc-call 
 
 def SetUpTable(cursor): # create necessary tables 
+    cursor.execute("""DROP TABLE IF EXISTS employee;""") # clean up before use 
     cursor.execute("""
         CREATE TABLE employee (
         staff_number INTEGER PRINMARY KEY, 
         fname VARCHAR(20)
         );"""
         )
-    cursor.execute("""DROP TABLE employee;""") # clean up after use 
     return; # return to line of fnc-call 
