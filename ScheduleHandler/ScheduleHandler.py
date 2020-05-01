f941d086d7df9e58bfa9533cc7c50089e8ee2db8
Léon Geide <leon.geide@mail.de>
2020-05-02 01:03:02 +0200

    testing add-colum functionality

--- ScheduleHandler/ScheduleHandler.py
+++ ScheduleHandler/ScheduleHandler.py
@@ -2,7 +2,7 @@ print("By now we can manipulate the names.") #
 
 import sqlite3 
 
-from dbHandler import connect_db, setup_table, add_column, add_entry, get_table 
+from dbHandler import connect_db, setup_table, add_entry, get_table 
 
 cursor = connect_db("any") 
 setup_table(cursor,"staff","staff_id","last_name") 
@@ -10,12 +10,18 @@ setup_table(cursor,"staff","staff_id","last_name")
 add_entry(cursor,"staff","William")
 add_entry(cursor,"staff")
 
-add_column(cursor,"staff","entry_date","TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL")
-add_column(cursor,"staff","position",)
+# add_column(cursor,"staff","entry_date","TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL")
+# add_column(cursor,"staff","position",)
+cursor.execute("""ALTER TABLE staff ADD 
+    entry_date TIMESTAMP;""") 
+    # NOT NULL DEFAULT CURRENT_TIMESTAMP won't work 
+    # because of the way sqlite handles the ALTER TABLE command 
+cursor.execute("""ALTER TABLE staff ADD
+    position VARCHAR(20);""") 
 add_entry(cursor,"staff","Jana")
 cursor.execute("""UPDATE staff 
-    SET entry_date = {} 
-    WHERE name = {} """
+    SET entry_date = '{}' 
+    WHERE last_name = '{}' """
     .format("2017-01-01","Jana") )
 
 get_table(cursor,"staff")
--- ScheduleHandler/dbHandler.py
+++ ScheduleHandler/dbHandler.py
@@ -18,7 +18,7 @@ def setup_table(target, table_name = "staff", index_field = "staff_id", name = "
     return; # return to line of fnc-call 
 
 def add_column(target,table_name = "staff", column_name = "dateof_entry", data_type = "VARCHAR(20)"): 
-    target.execute("""ALTER TABLE {} ADD IF NOT EXISTS {} {};""".format(table_name,column_name,data_type) ) 
+#    target.execute("""ALTER TABLE {} ADD {} {};""".format(table_name,column_name,data_type) ) 
     return; 
 
 def add_entry(target,table_name = "staff", name = "TestUser1"):
