4c3fbe76e9ea2105939cb0c795859c54709a98b6
Léon Geide <leon.geide@mail.de>
2020-05-01 03:10:31 +0200

    use string templates with {}

--- ScheduleHandler/dbHandler.py
+++ ScheduleHandler/dbHandler.py
@@ -1,30 +1,33 @@
 import sqlite3 
 
 def connect_db(db_name = "any"): # creates db if not existing 
-    connection = sqlite3.connect("'%s'.db" % db_name)  
+    connection = sqlite3.connect("{}.db".format(db_name))  
     cursor = connection.cursor() 
     return cursor; # return value of 'cursor' to line of fnc-call 
 
 def setup_table(target, table_name = "staff", index_field = "staff_id", name = "last_name"): 
     # pass variables with default value 
     # very basic way, prone to SQL-injections 
-    target.execute("""DROP TABLE IF EXISTS '%s'""" % table_name) 
+    target.execute("""DROP TABLE IF EXISTS {}""".format(table_name) ) 
     target.execute("""
-        CREATE TABLE IF NOT EXISTS '%s' (
-        '%s' INTEGER PRIMARY KEY, 
-        '%s' VARCHAR(20)
-        );""" % (table_name,index_field,name)
+        CREATE TABLE IF NOT EXISTS {} (
+        {} INTEGER PRIMARY KEY, 
+        {} VARCHAR(20)
+        );""" .format(table_name,index_field,name)
         )
     return; # return to line of fnc-call 
 
 def add_entry(target,table_name = "staff", name = "TestUser1"):
-    target.execute("""INSERT INTO '%(table_name)s' (staff_id,last_name)
-        VALUES (NULL, '%(name)s');""" % { 'table_name': table_name, 'name': name} )
+    target.execute("""INSERT INTO {} (staff_id,last_name)
+        VALUES (NULL, '{}');"""
+        .format(table_name,name) )
     return; 
 
 def get_table(target,table_name = "staff"): 
-    target.execute("SELECT * FROM '%s'" % table_name) 
-    print("Table '%s':" % table_name) 
+    target.execute("SELECT * FROM {}"
+        .format(table_name) ) 
+    print("Table {}:"
+        .format(table_name) ) 
     result = target.fetchall() 
     for r in result:
         print(r)
