79018836610fe50f026f72b7a517a5fad9152352
Léon Geide <leon.geide@mail.de>
2020-05-01 02:28:31 +0200

    adding output-option

--- ScheduleHandler/ScheduleHandler.py
+++ ScheduleHandler/ScheduleHandler.py
@@ -2,7 +2,8 @@ print("By now we can manipulate the names.") #
 
 import sqlite3 
 
-from dbHandler import ConnectDB, SetUpTable
+from dbHandler import ConnectDB, SetUpTable, GetTable 
 
 cursor = ConnectDB("any") 
 SetUpTable(cursor,"staff","staff_id","last_name") 
+GetTable(cursor,"staff")
--- ScheduleHandler/dbHandler.py
+++ ScheduleHandler/dbHandler.py
@@ -5,13 +5,21 @@ def ConnectDB(db_name = "any"): # creates db if not existing
     cursor = connection.cursor() 
     return cursor; # return value of 'cursor' to line of fnc-call 
 
-def SetUpTable(cursor, table_name = "staff", index_field = "staff_id", name = "last_name"): 
+def SetUpTable(target, table_name = "staff", index_field = "staff_id", name = "last_name"): 
     # pass variables with default value 
     # very basic way, prone to SQL-injections 
-    cursor.execute("""
+    target.execute("""
         CREATE TABLE IF NOT EXISTS """ + table_name + """(
         """ + index_field + """ INTEGER PRIMARY KEY, 
         """ + name + """ VARCHAR(20)
         );"""
         )
     return; # return to line of fnc-call 
+
+def GetTable(target,table_name = "staff"): 
+    target.execute("SELECT * FROM " + table_name) 
+    print("Table " + table_name + ":") 
+    result = target.fetchall() 
+    for r in result:
+        print(r)
+    return; 
